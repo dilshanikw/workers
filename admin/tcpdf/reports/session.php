b63f0381672de5751656bdd052204017a69a061d
dilshanikw <81347359+dilshanikw@users.noreply.github.com>
2021-09-04 11:15:35 +0530

    Add files via upload

---
+++ admin/tcpdf/reports/session.php
@@ -0,0 +1,23 @@
+
+<?php
+include 'connect.php';
+session_start();
+	$ses_user=$_SESSION['login_user'];
+	$ses_level=$_SESSION['user_level'];
+	$ses_ukey=$_SESSION['user_key'];
+	
+	$sql9999="SELECT * FROM coordinator WHERE codinator_key='$ses_ukey'";
+	$result9999=mysqli_query($link,$sql9999);
+	$userinfo9999=mysqli_fetch_assoc($result9999);
+	$uadmin_first=$userinfo9999['first_name'];
+	$uadmin_last=$userinfo9999['lastname'];
+	$uprofilepic=$userinfo9999['profile_pic'];
+
+	$uadmin_fulln=$uadmin_first." ".$uadmin_last;
+	
+	if(!isset($_SESSION['login_user'])){
+		header("location:../index.php");
+	}
+
+
+?>
